1eb0ffb3897b4adccf090bf77cbc1cbbec0d5694
RickyAnchores <ricky23anchores@gmail.com>
2021-09-12 19:15:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>MinARK</h3>
+      <p>minimalist and architecture</p>
+    </div>
+  )
+}
+
+export default App
+
